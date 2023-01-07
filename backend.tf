2d0b7571f5ac92f0bdd6482b3c3376cbc2faa0a6
sankarb91 <121793057+sankarb91@users.noreply.github.com>
2023-01-07 21:22:42 +0530

    backend.tf
    backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+ terraform {
+    backend "s3" {
+      bucket = "my-dev-tf-state-bucket"
+      key = "main"
+      region = "us-east-2"
+      dynamodb_table = "my-dynamodb_table"
+     }
+   }   		
+	  
