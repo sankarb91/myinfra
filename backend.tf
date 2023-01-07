88953a2f90b7a3d6e1bb9e1c724dc77204fc7344
sankarb91 <121793057+sankarb91@users.noreply.github.com>
2023-01-07 22:40:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
       bucket = "my-dev-tf-state-bucket1"
       key = "main"
       region = "us-east-2"
-      dynamodb_table = "my-dynamodb_table1"
+      dynamodb_table = "my-dynamodb_table"
      }
    }   		
 	  
